6b58804884df90865da741b926837e91ee791b77
Laubfrosch24 <Laubfrosch24@users.noreply.github.com>
2018-01-17 20:22:48 +0100

    Karten Ziehen

--- UNO/Model/Spielfeld.cs
+++ UNO/Model/Spielfeld.cs
@@ -17,9 +17,11 @@ namespace UNO.Model
         List<IKarte> GelegteKarten = new List<IKarte>();
         ISpieler AktiverSpieler;
         bool NichtGelegt = true;
+        int KartenZiehen;
 
         public Spielfeld(IEnumerable<ISpieler> spieler)
         {
+            KartenZiehen = 0;
             Spieler = spieler.ToDictionary(s => s.Socket, s => s);
             InitStapel();
         }
@@ -44,6 +46,18 @@ namespace UNO.Model
             while (stopWatch.ElapsedMilliseconds < 20000 && NichtGelegt)
             {
                 
+            }
+            if (GelegteKarten.Last().Typ == KartenTyp.Ziehen)
+            {
+                KartenZiehen += 2;
+            }
+            else
+            {
+                for (int i = 0; i < KartenZiehen; i++)
+                {
+                    AktiverSpieler.Karten.Add(Stapel.Dequeue());
+                }
+                KartenZiehen = 0;
             }
             stopWatch.Stop();
             Spieler.Remove(key);
