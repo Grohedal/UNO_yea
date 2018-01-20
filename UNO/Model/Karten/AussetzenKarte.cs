b8aaec0e5b5e896810bd4e0bfcfdf59bc15b995f
Laubfrosch24 <Laubfrosch24@users.noreply.github.com>
2018-01-20 21:47:07 +0100

    Richtungswechsel und Aussetzen hinzugefügt (funzt, todo Oberfläche bearbeiten)

---
+++ UNO/Model/Karten/AussetzenKarte.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNO.Model.Karten
+{
+    class AussetzenKarte:IKarte
+    {
+        KartenTyp IKarte.Typ => KartenTyp.Aussetzen;
+
+        public KartenFarbe Farbe { get; }
+
+        public AussetzenKarte(KartenFarbe farbe)
+        {
+            Farbe = farbe;
+        }
+    }
+}
---
+++ UNO/Model/Karten/RichtungswechselKarte.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNO.Model.Karten
+{
+    class RichtungswechselKarte :IKarte
+    {
+        KartenTyp IKarte.Typ => KartenTyp.Richtungswechsel;
+
+        public KartenFarbe Farbe { get; }
+
+        public RichtungswechselKarte(KartenFarbe farbe)
+        {
+            Farbe = farbe;
+        }
+    }
+}
